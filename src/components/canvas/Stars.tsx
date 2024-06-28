fe19719a7b0525f775581c4f65aca5f9b2bce37f
Ezgi <ezgidemir824@gmail.com>
2024-06-28 20:18:37 +0300

    performance

--- src/components/canvas/Stars.tsx
+++ src/components/canvas/Stars.tsx
@@ -1,15 +1,15 @@
-import React, { useRef, Suspense } from "react";
+import { useState, useRef, Suspense } from "react";
 import { Canvas, useFrame } from "@react-three/fiber";
 import { Points, PointMaterial, Preload } from "@react-three/drei";
 import { random } from "maath";
+import { TypedArray } from "three";
 
-const Stars = () => {
-  const ref = useRef<THREE.Points>(null); // Ref for the Points object
+const Stars = (props: any) => {
+  const ref = useRef<THREE.Points>();
+  const [sphere] = useState<TypedArray>(() =>
+    random.inSphere(new Float32Array(5001), { radius: 1.2 })
+  );
 
-  // Generate random positions for stars using useMemo
-  const sphere = random.inSphere(new Float32Array(5001), { radius: 1.2 });
-
-  // Animation loop using useFrame to rotate the stars
   useFrame((_state, delta) => {
     if (ref.current) {
       ref.current.rotation.x -= delta / 10;
@@ -19,7 +19,7 @@ const Stars = () => {
 
   return (
     <group rotation={[0, 0, Math.PI / 4]}>
-      <Points ref={ref} positions={sphere} stride={3} frustumCulled>
+      <Points ref={ref} positions={sphere} stride={3} frustumCulled {...props}>
         <PointMaterial
           transparent
           color="#f272c8"
@@ -35,14 +35,11 @@ const Stars = () => {
 const StarsCanvas = () => {
   return (
     <div className="absolute inset-0 z-[-1] h-auto w-full">
-      <Canvas
-        dpr={Math.min(window.devicePixelRatio, 2)}
-        gl={{ antialias: true, powerPreference: "high-performance" }}
-        camera={{ position: [0, 0, 1] }}
-      >
+      <Canvas camera={{ position: [0, 0, 1] }}>
         <Suspense fallback={null}>
           <Stars />
         </Suspense>
+
         <Preload all />
       </Canvas>
     </div>
